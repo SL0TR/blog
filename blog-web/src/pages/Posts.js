3fe523d523782b4dc4b5a1e92594b2d48a0c5744
SL0TR <mohaiminislam94@gmail.com>
2021-11-04 22:07:06 +0600

    chore: add pagination with backend api

--- blog-web/src/pages/Posts.js
+++ blog-web/src/pages/Posts.js
@@ -1,4 +1,4 @@
-import { Col, Row, Typography } from "antd";
+import { Col, Pagination, Row, Typography } from "antd";
 import { postsUrl } from "api/endpoints";
 import SinglePostCard from "component/PostCard";
 import { useEffect, useState } from "react";
@@ -6,20 +6,28 @@ import http from "services/httpService";
 
 function Posts() {
   const [posts, setPosts] = useState([]);
+  const [totalPosts, setTotalPosts] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 6;
 
   console.log(posts);
 
-  async function fetchPosts() {
-    const res = await http.get(postsUrl);
-
-    if (res?.data?.data) {
-      setPosts(res?.data?.data);
-    }
-  }
-
   useEffect(() => {
+    async function fetchPosts() {
+      const offset = (currentPage - 1) * pageSize;
+
+      const res = await http.get(
+        `${postsUrl}?limit=${pageSize}${offset ? `&offset=${offset}` : ""}`
+      );
+
+      if (res?.data?.data) {
+        setPosts(res?.data?.data);
+        setTotalPosts(res?.data?.totalCount);
+      }
+    }
+
     fetchPosts();
-  }, []);
+  }, [currentPage]);
 
   return (
     <Row
@@ -29,17 +37,27 @@ function Posts() {
       justify="center"
       align="middle"
     >
-      <Col className="gutter-row" span={24} align="middle">
+      <Col span={24} align="middle">
         <Typography.Title>Posts</Typography.Title>
       </Col>
-      <Col className="gutter-row" span={20}>
+      <Col span={20}>
         <Row gutter={[50, 50]}>
           {posts.map((post) => (
-            <Col className="gutter-row" span={8} key={post._id}>
+            <Col span={8} key={post._id}>
               <SinglePostCard post={post} />
             </Col>
           ))}
         </Row>
+      </Col>
+      <Col span={20}>
+        <Pagination
+          defaultCurrent={1}
+          current={currentPage}
+          total={totalPosts}
+          pageSize={pageSize}
+          onChange={(page) => setCurrentPage(page)}
+        />
+        ,
       </Col>
     </Row>
   );
